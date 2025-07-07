fa6ce823ca317211e2f47be8618f20240ee81cd2
randomideas77 <cannoli_patsies6c@icloud.com>
2025-07-07 17:56:54 -0500

    mailbox work
    implemented mailbox into main.c, added a function to get the board revision number and than flash that number using the binary flasher function. added "size" variable to the binary flasher function so that the least significant bit can be changed when called.

---
+++ main.h
@@ -0,0 +1,21 @@
+// main.h 0.0.4
+#ifndef MAIN_H
+#define MAIN_H
+
+// GPIO setup
+#define GPIO_BASE 0x3F200000  // For Pi 3B+ (BCM2837)
+#define GPFSEL2   ((volatile unsigned int*)(GPIO_BASE + 0x08))
+#define GPSET0    ((volatile unsigned int*)(GPIO_BASE + 0x1C))
+#define GPCLR0    ((volatile unsigned int*)(GPIO_BASE + 0x28))
+
+// Mailbox setup
+#define MMIO_BASE       0x3F000000
+#define MAILBOX_BASE    (MMIO_BASE + 0xB880)
+#define MAILBOX_READ    ((volatile unsigned int*)(MAILBOX_BASE + 0x0))
+#define MAILBOX_STATUS  ((volatile unsigned int*)(MAILBOX_BASE + 0x18))
+#define MAILBOX_WRITE  ((volatile unsigned int*)(MAILBOX_BASE + 0x20))
+#define MAILBOX_EMPTY   0x40000000
+#define MAILBOX_FULL   0x80000000
+#define MAILBOX_TIMEOUT_ERROR 0xFFFFFFFF
+
+#endif
